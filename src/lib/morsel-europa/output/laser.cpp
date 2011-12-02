e46b88f094d15a0713d466fce0ee7365622ae4f4
Jerome Maye <jerome.maye@mavt.ethz.ch>
2011-12-02 14:57:50 +0000

    laser updated

--- src/lib/morsel-europa/output/laser.cpp
+++ src/lib/morsel-europa/output/laser.cpp
@@ -43,21 +43,28 @@ Laser::~Laser() {
 /******************************************************************************/
 
 void Laser::publish(double time) {
+  const LVecBase2f& resolution = mSensor.getResolution();
+  const LVecBase2f& rangeLimits = mSensor.getRangeLimits();
+  const LVecBase2f& minAngles = mSensor.getMinAngles();
+  const LVecBase2f& maxAngles = mSensor.getMaxAngles();
+  static size_t counter = 0;
   LaserMsg msg;
   msg.type = "SICK_LMS_100";
   msg.timestamp = MOOSTime();
-  msg.resolution = 0.5;
-  msg.minAngle = -90.25;
-  msg.maxAngle = msg.minAngle + msg.resolution * (mSensor.getNumRays() - 1);
+  msg.resolution = resolution[0] * 180.0 / M_PI;
+  msg.minAngle = (minAngles[0] + 0.5 * resolution[0]) * 180.0 / M_PI;
+  msg.maxAngle = (maxAngles[0] - 0.5 * resolution[0]) * 180.0 / M_PI;
   msg.offset = 0.0;
-  msg.maxRange = 15;
-  msg.id++;
-  msg.count++;
-  msg.range.clear();
+  msg.maxRange = rangeLimits[1];
+  msg.id = counter;
+  msg.count = counter++;
   for (size_t i = 0; i < mSensor.getNumRays(); ++i) {
     RangeSensor::Ray ray = mSensor.getRay(i);
-    msg.range.push_back(std::sqrt(ray.point[0] * ray.point[0] +
-      ray.point[1] * ray.point[1] + ray.point[2] * ray.point[2]));
+    if (ray.valid)
+      msg.range.push_back(std::sqrt(ray.point[0] * ray.point[0] +
+        ray.point[1] * ray.point[1] + ray.point[2] * ray.point[2]));
+    else
+      msg.range.push_back(-1);
   }
   if (!publishString(msg.toString()))
     std::cerr << "Laser::publish(): failed to publish on MOOS" << std::endl;
