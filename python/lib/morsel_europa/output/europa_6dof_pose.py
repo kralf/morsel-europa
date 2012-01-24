826ecae7e40ea113d5e0206898256b2f94ae6ba3
Ralf Kaestner <ralf.kaestner@gmail.com>
2012-01-24 08:46:41 +0000

    Added 6DOF pose message to Morsel

---
+++ python/lib/morsel_europa/output/europa_6dof_pose.py
@@ -0,0 +1,44 @@
+################################################################################
+# Copyright (C) 2011 by Jerome Maye                                            #
+# jerome.maye@gmail.com                                                        #
+#                                                                              #
+# This program is free software; you can redistribute it and/or modify         #
+# it under the terms of the Lesser GNU General Public License as published by  #
+# the Free Software Foundation; either version 3 of the License, or            #
+# (at your option) any later version.                                          #
+#                                                                              #
+# This program is distributed in the hope that it will be useful,              #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+# Lesser GNU General Public License for more details.                          #
+#                                                                              #
+# You should have received a copy of the Lesser GNU General Public License     #
+# along with this program. If not, see <http://www.gnu.org/licenses/>.         #
+################################################################################
+
+from morsel.nodes import Output, Node
+from morsel_europa.europac import Europa6DOFOdometry as CEuropa6DOFOdometry
+
+#-------------------------------------------------------------------------------
+
+class Europa6DOFPose(Output):
+  def __init__(self, client, platform, name = "6DOFPose", message = "",
+      **kargs):
+    Output.__init__(self, name, **kargs)
+
+    self.client = client
+    self.platform = platform
+    self.message = message
+
+    self.publisher = CEuropa6DOFOdometry(name, self.client.client,
+      self.message)
+    self.publisher.reparentTo(self)
+
+#-------------------------------------------------------------------------------
+
+  def outputData(self, time):
+    position = self.platform.getPosition(self.world)
+    orientation = self.platform.getOrientation(self.world)
+
+    self.publisher.publish(time, framework.scheduler.frameTime,
+      panda.Vec3(*position), panda.Vec3(*orientation))
